ef2485f5165f93090bbef72e302d933231d7170f
f2xiao <fxiao.mecheng@gmail.com>
2021-10-15 19:00:25 -0400

    Add Coding Challenge 2

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -2,6 +2,7 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
+const images = document.querySelector('.images');
 
 ///////////////////////////////////////
 // Our First AJAX Call: XMLHttpRequest
@@ -132,10 +133,99 @@ const whereAmI = (lat, lng) => {
     });
 };
 
-btn.addEventListener('click', () => {
+/* btn.addEventListener('click', () => {
   whereAmI(52.508, 13.381).then(data => {
     const { city, country } = data;
     console.log(`You are in ${city}, ${country}`);
     getCountryData(country, `https://restcountries.com/v3.1/name/${country}`);
   });
 });
+ */
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+If this part is too tricky for you, just watch the first part of the solution.
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+GOOD LUCK 😀
+*/
+
+function createImage(imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+    // load successfully
+    img.addEventListener('load', event => {
+      images.appendChild(img);
+      resolve(img);
+    });
+
+    // load failed
+
+    img.addEventListener('error', event => {
+      reject(new Error('image not found'));
+    });
+  });
+}
+
+/* let imgPath = `./img/img-1.jpg`;
+console.log(imgPath); */
+/* const img = document.createElement('img');
+img.src = imgPath;
+// load successfully
+img.addEventListener('load', event => {
+  images.appendChild(img);
+}); */
+let currentImg;
+const wait = function (seconds) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+// wait(1)
+//   .then(res => {
+//     console.log('1s has passed');
+//     return wait(1);
+//   })
+//   .then(res => {
+//     console.log('2s has passed');
+//   });
+
+createImage(`./img/img-1.jpg`)
+  .then(img => {
+    currentImg = img;
+    return wait(2);
+  })
+  .then(res => {
+    currentImg.style.display = 'none';
+    return createImage(`./img/img-2.jpg`);
+  })
+  .then(img => {
+    currentImg = img;
+    return wait(2);
+  })
+  .then(res => {
+    currentImg.style.display = 'none';
+    return createImage(`./img/img-3.jpg`);
+  })
+  .then(img => {
+    currentImg = img;
+    return wait(2);
+  })
+  .then(res => {
+    currentImg.style.display = 'none';
+  })
+  .catch(error => {
+    images.insertAdjacentHTML('beforeend', `ohhh no 💥💥💥${error.message}`);
+  });
