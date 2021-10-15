184d77054e96d71a6d1dd61348fe290271117a1a
f2xiao <fxiao.mecheng@gmail.com>
2021-10-15 01:00:36 -0400

    Add error handling for fetch APIs

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,7 +4,11 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-// AJAX Call
+// Our First AJAX Call: XMLHttpRequest
+// Consuming Promises
+// Chaining Promises
+// Handling Rejected Promises
+// Throwing Errors Manually
 
 function renderCountry(data, className = '') {
   const html = `<article class="country ${className}">
@@ -23,13 +27,25 @@ function renderCountry(data, className = '') {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
 }
+function renderError(errMsg) {
+  countriesContainer.insertAdjacentText('beforeend', errMsg);
+  countriesContainer.style.opacity = 1;
+}
+
+function getJSON(APIurl) {
+  fetch(APIurl).then(response => {
+    if (!response.ok) throw new Error('can not find the country');
+    return response.json();
+  });
+}
 
 function getCountryData(country, APIurl) {
-  fetch(APIurl)
-    .then(response => {
-      if (!response.ok) throw new Error('Something went wrong');
-      return response.json();
-    })
+  // fetch(APIurl)
+  //   .then(response => {
+  //     if (!response.ok) throw new Error('Something went wrong');
+  //     return response.json();
+  //   })
+  getJSON(APIurl)
     .then(data => {
       if (data.length !== 1) {
         data = data.filter(ele => ele.name.common == country);
@@ -47,11 +63,37 @@ function getCountryData(country, APIurl) {
       if (!response.ok) throw new Error('Something went wrong');
       return response.json();
     })
-    .then(data => renderCountry(data[0], 'neighbour'));
+    .then(data => renderCountry(data[0], 'neighbour'))
+    .catch(error => {
+      console.error(`${error} 💥💥💥`);
+      renderError(`💥💥oh no, ${error.message} 💥💥`);
+    });
 }
 
 btn.addEventListener('click', () => {
-  const country = 'afa';
+  const country = 'aaa';
   const APIurl = `https://restcountries.com/v3.1/name/${country}`;
   getCountryData(country, APIurl);
 });
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+Here are your tasks:
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+GOOD LUCK 😀
+*/
