f972f1808f3799e594f44bcbe8968bfa972159fb
f2xiao <fxiao.mecheng@gmail.com>
2021-10-17 16:50:07 -0400

    Add Coding Challenge 3

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -222,7 +222,7 @@ img.src = imgPath;
 img.addEventListener('load', event => {
   images.appendChild(img);
 }); */
-let currentImg;
+// let currentImg;
 const wait = function (seconds) {
   return new Promise(function (resolve, reject) {
     setTimeout(resolve, seconds * 1000);
@@ -378,20 +378,20 @@ const get3Countries = async function (c1, c2, c3) {
     console.log(d[0].capital[0]);
   });
 };
-get3Countries('China', 'Canada', 'USA');
+// get3Countries('China', 'Canada', 'USA');
 
 ///////////////////////////////////////
 // Other Promise Combinators: race, allSettled and any
 // Promise.race()
 
-(async function (c1 = 'China', c2 = 'Canada', c3 = 'USA') {
+/* (async function (c1 = 'China', c2 = 'Canada', c3 = 'USA') {
   const data = await Promise.race([
     getJSON(`https://restcountries.com/v3.1/name/${c1}`),
     getJSON(`https://restcountries.com/v3.1/name/${c2}`),
     getJSON(`https://restcountries.com/v3.1/name/${c3}`),
   ]);
   console.log(data);
-})();
+})(); */
 
 const timeout = function (sec) {
   return new Promise((_, reject) => {
@@ -401,7 +401,7 @@ const timeout = function (sec) {
   });
 };
 
-Promise.race([getJSON(`https://restcountries.com/v3.1/name/China`), timeout(2)])
+/* Promise.race([getJSON(`https://restcountries.com/v3.1/name/China`), timeout(2)])
   .then(res => {
     console.log(res);
   })
@@ -452,4 +452,43 @@ Promise.any([
   })
   .catch(err => {
     console.log(err);
+  }); */
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+GOOD LUCK 😀
+*/
+
+let currentImg;
+const loadNPause = async function (imgPath) {
+  currentImg = await createImage(imgPath);
+  console.log(currentImg);
+  await wait(2);
+  currentImg.style.display = 'none';
+};
+
+// loadNPause(`./img/img-1.jpg`);
+
+const loadAll = async function (imgArr) {
+  // for (let index = 0; index < imgArr.length; index++) {
+  //   loadNPause(imgArr[index]);
+  // }
+  let imgs = [];
+  imgArr.map(async function (imgPath, index) {
+    imgs[index] = await loadNPause(imgPath);
   });
+};
+let imgArr = [`./img/img-1.jpg`, `./img/img-2.jpg`, `./img/img-3.jpg`];
+loadAll(imgArr);
