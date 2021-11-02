ce3bc14658eda80a2c14ac3eef9fb457b6e7b0cd
f2xiao <fxiao.mecheng@gmail.com>
2021-11-01 20:15:19 -0400

    Add Spread Operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -7,15 +7,14 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  orderFood: function (mainIndex = 1, starterIndex = 0) {
+  orderDelivery({ mainIndex = 1, starterIndex = 0 } = {}) {
     // console.log(mainIndex, starterIndex);
     console.log(
-      `main: ${restaurant.mainMenu[mainIndex]} and starter: ${restaurant.starterMenu[starterIndex]} are added to order.`
+      `starter: ${this.starterMenu[starterIndex]} and main: ${this.mainMenu[mainIndex]} are added to order.`
     );
-    return [
-      restaurant.mainMenu[mainIndex],
-      restaurant.starterMenu[starterIndex],
-    ];
+  },
+  orderPasta(ing1, ing2, ing3) {
+    console.log(`Pasta with ${ing1}, ${ing2} and ${ing3} is ordered`);
   },
 
   openingHours: {
@@ -37,7 +36,7 @@ const restaurant = {
 //////////////////////////////////////////////////////////////////////
 // Array Destructuring
 
-// Basic Assignment
+/* // Basic Assignment
 let [a, b] = restaurant.starterMenu;
 console.log(a, b);
 
@@ -75,21 +74,31 @@ console.log(main);
 [,] = restaurant.orderFood();
 
 // Unpacking values from a regular expression match
-// RegExp.prototype.exec()
+// RegExp.prototype.exec() */
 
 //////////////////////////////////////////////////////////////////////
 // Object Destructuring
 
-// Basic assignment
+/* // Basic assignment
 const { fri: Friday } = restaurant.openingHours;
 console.log(Friday);
 // Assignment without declaration
+let catName, catAge;
+({ name: catName, age: catAge } = { name: 'Pixie', age: 8 });
 // Assigning to new variable names
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
 // Default values
 // Assigning to new variables names and providing default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
 
 // Unpacking fields from objects passed as a function parameter
-
+restaurant.orderDelivery();
 // Setting a function parameter's default value
 function drawES2015Chart({
   size = 'big',
@@ -111,5 +120,101 @@ console.log(size, cords, radius);
 const {
   fri: { open, close },
 } = restaurant.openingHours;
-console.log(open, close);
-// For of iteration and destructuring
+console.log(open, close); */
+
+///////////////////////////////////////
+// The Spread Operator (...)
+/* // Create a new array with an existeing array
+const arr = [3, 4, 5];
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+// Copy array, shallow copy
+const arr1 = [
+  1,
+  {
+    name: 'Pixie',
+    categories: {
+      home: 'waterloo',
+    },
+  },
+  3,
+];
+const arrCopy = [...arr1];
+console.log(arrCopy);
+console.log(arrCopy === arr1);
+
+// Join 2 arrays
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Pixie';
+const letters = [...str, ' ', 'S.'];
+console.log(letters);
+console.log(...str);
+console.log(...str, ` The Ruler`);
+// Real-world example
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3?'),
+// ];
+// restaurant.orderPasta(...ingredients);
+// Objects
+
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Nick' };
+console.log(newRestaurant);
+console.log(restaurant); */
+
+///////////////////////////////////////
+// Rest Pattern and Parameters
+// 1) Destructuring
+// SPREAD, because on RIGHT side of =
+// REST, because on LEFT side of =
+// Objects
+// 2) Functions
+
+///////////////////////////////////////
+// Short Circuiting (&& and ||)
+console.log('---- OR ----');
+
+// Use ANY data type, return ANY data type, short-circuiting
+console.log('---- AND ----');
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK 😀
+*/
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+GOOD LUCK 😀
+*/
