3b01ac658d23de2006cc772ad5abf0edae830172
f2xiao <fxiao.mecheng@gmail.com>
2021-11-03 01:12:05 -0400

    Add showRecipe in controller.js

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,3 +11,18 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const obj = {a:1,b:2};
+console.log(obj, typeof obj);
+const showRecipe = async function () {
+  try {
+    let res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'); // returns a response object
+    if(!res.ok) throw new Error(`Something is wrong ${res.status}`); // will stop execution of the code below?
+    const {data} = await res.json(); // parse the response body data from the response object
+    const {recipe} = data;
+    console.log(recipe);
+    
+  } catch (error) {
+    console.error(`${error} 💥💥💥`);
+  }
+}
+showRecipe();
